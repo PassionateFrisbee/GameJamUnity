6623f9746054cbfb676b7c4211d2350729edd1d1
VioletShi-_- <passionatefrisbee@gmail.com>
2023-02-07 12:57:07 +0100

    dotted line?

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -4,30 +4,36 @@ using UnityEngine;
 
 public class Door : MonoBehaviour
 {
-    // Start is called before the first frame update
 
     public bool isOpen = false;
     private BoxCollider2D bc;
+    private SpriteRenderer sr;
 
-    public void toggleDoor() {
-        if(!isOpen) {
+    void Start()
+    {
+        bc = gameObject.GetComponent<BoxCollider2D>();
+        sr = GetComponent<SpriteRenderer>();
+    }
+
+    void Update()
+    {
+        if (isOpen) { sr.enabled = false; }
+        else { sr.enabled = true; }
+    }
+
+    public void toggleDoor()
+    {
+        if (!isOpen)
+        {
             isOpen = true;
             bc.isTrigger = true;
             transform.position = new Vector3(transform.position.x, transform.position.y, 1);
-        } else {
+        }
+        else
+        {
             isOpen = false;
             bc.isTrigger = false;
             transform.position = new Vector3(transform.position.x, transform.position.y, 0);
         }
-    }
-    void Start()
-    {
-        bc = gameObject.GetComponent<BoxCollider2D>();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
--- Assets/Scripts/WaveFlag.cs
+++ Assets/Scripts/WaveFlag.cs
@@ -3,56 +3,30 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class WaveFlag : MonoBehaviour
-{
-    public Sprite sprite1;
-    public Sprite sprite2;
-    public Sprite sprite3;
-    public Sprite sprite4;
-    public Sprite sprite5;
-    public Sprite sprite6;
+{ 
+    private SpriteRenderer sr;
+    private int frameIndex;
+    public Sprite[] frames;
+    public float fps = 3;
+    private float sliderTimer;
 
-    private SpriteRenderer mySpriteRenderer;    
-
-    public float spriteTimer = .1f;
-
-    private float timer;
-
-    void changeSprite() {
-        if(mySpriteRenderer.sprite == sprite1) {
-            mySpriteRenderer.sprite = sprite2;
-        }
-        else if(mySpriteRenderer.sprite == sprite2) {
-            mySpriteRenderer.sprite = sprite3;
-        }
-        else if(mySpriteRenderer.sprite == sprite3) {
-            mySpriteRenderer.sprite = sprite4;
-        }
-        else if(mySpriteRenderer.sprite == sprite4) {
-            mySpriteRenderer.sprite = sprite5;
-        }
-        else if(mySpriteRenderer.sprite == sprite5) {
-            mySpriteRenderer.sprite = sprite6;
-        }
-        else if(mySpriteRenderer.sprite == sprite6) {
-            mySpriteRenderer.sprite = sprite1;
-        }
-    }
-
-    
-    // Start is called before the first frame update
     void Start()
     {
-        timer = spriteTimer;
-        mySpriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+        sr = GetComponent<SpriteRenderer>();
+        sliderTimer = (1f / fps);
+        frameIndex = 0;
     }
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        timer -= Time.deltaTime;
-        if(timer <= 0) {
-            changeSprite();
-            timer = spriteTimer;
+        sliderTimer -= Time.deltaTime;
+        if (sliderTimer <= 0)
+        {
+            frameIndex++;
+            if (frameIndex >= frames.Length) { frameIndex = 0; }
+
+            sliderTimer = (1f / fps);
+            sr.sprite = frames[frameIndex];
         }
     }
 }
